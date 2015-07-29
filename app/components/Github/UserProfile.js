4274075f016b7af40cac6c9e1274bf74a466259e
Daniel Leibovic <djleibovic@gmail.com>
2015-07-28 23:43:30 -0400

    added routes and props passing and propType validation

---
+++ app/components/Github/UserProfile.js
@@ -0,0 +1,21 @@
+var React = require('react');
+
+var UserProfiles = React.createClass({
+	propTypes: {
+		username: React.PropTypes.string.isRequired,
+		bio: React.PropTypes.object.isRequired
+	},
+	
+	render: function() {
+		return(
+			<div>
+				UserProfile <br />
+				Username: {this.props.username} <br />
+				Bio: {this.props.bio}
+			</div>
+			
+		)
+	}
+});
+
+module.exports = UserProfiles;
